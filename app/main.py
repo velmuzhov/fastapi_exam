7c470fc0ad6cee666a5d2ff99379a1bc2e98967f
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-09-25 21:56:00 +0300

    categories router included in app in main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 
+from app.routers import categories
+
 
 app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
+
+# Подключение маршрутов категорий
+app.include_router(categories.router)
 
 
 # Корневой endpoint для проверки
