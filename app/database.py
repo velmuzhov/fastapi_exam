d6f2cdefa17b542030f2ca2170c67bc158faec01
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-09-27 12:15:35 +0300

    Product model added with mapped_column

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine, Engine, String
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 
 
 DATABASE_URL: str = "sqlite:///ecommerce.db"
@@ -14,3 +14,12 @@ SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(200))
+    price: Mapped[float] = mapped_column(nullable=False)
