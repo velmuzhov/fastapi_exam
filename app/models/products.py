fdf123b9eb9520cfa1b0e5d8f7367fbe7d7393cf
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-09-28 10:17:37 +0300

    Product table created

---
+++ app/models/products.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+from sqlalchemy import String, Integer, Numeric
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(500), nullable=True)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False) # переделал на другой тип
+    image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
+    stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
