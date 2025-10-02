6878926d48e669b7d984e7c61918d50fbdd71ce8
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-10-02 20:47:39 +0300

    self-link in categories model added

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -13,8 +13,22 @@ class Category(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
+    parent_id: Mapped[int | None] = mapped_column(
+        ForeignKey("categories.id"), nullable=True
+    )
     is_active: Mapped[bool] = mapped_column(default=True)
 
     products: Mapped[list["Product"]] = relationship(
         "Product", back_populates="category"
+    )
+
+    parent: Mapped["Category | None"] = relationship(
+        "Category",
+        back_populates="children",
+        remote_side="Category.id",
+    )
+
+    children: Mapped[list["Category"]] = relationship(
+        "Category",
+        back_populates="parent",
     )
