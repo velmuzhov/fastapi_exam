e98d7345640d14c68cee137c21c8d0dceed940f4
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-10-10 08:38:41 +0300

    database set up for async postgresql

--- app/database.py
+++ app/database.py
@@ -1,15 +1,16 @@
-from sqlalchemy import create_engine, Engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:1234@localhost:5750/ecommerce_db"
 
-DATABASE_URL: str = "sqlite:///ecommerce.db"
-
-engine: Engine = create_engine(
+async_engine = create_async_engine(
     url=DATABASE_URL,
     echo=True,
 )
 
-SessionLocal = sessionmaker(bind=engine)
+async_session_maker = async_sessionmaker(
+    async_engine, expire_on_commit=False, class_=AsyncSession
+)
 
 
 class Base(DeclarativeBase):
