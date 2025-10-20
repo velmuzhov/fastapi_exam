2ccc874c8164c05fe4b44fa84f55f81ffc72023f
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-10-20 13:11:15 +0300

    user model added

---
+++ app/models/users.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+if TYPE_CHECKING:
+    from .products import Product
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+    role: Mapped[str] = mapped_column(String, default="buyer")
+
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="seller")
