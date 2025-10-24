1c53c81807ea98f8175b3778f68b302738df7eb7
Vladimir Velmuzhov <velmuzhov@gmail.com>
2025-10-24 21:14:17 +0300

    reviews model created

--- app/models/products.py
+++ app/models/products.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 from decimal import Decimal
-from sqlalchemy import String, Integer, Numeric, ForeignKey
+from sqlalchemy import String, Integer, Numeric, ForeignKey, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -8,6 +8,7 @@ from app.database import Base
 if TYPE_CHECKING:
     from app.models.categories import Category
     from app.models.users import User
+    from app.models.reviews import Review
 
 
 class Product(Base):
@@ -18,9 +19,10 @@ class Product(Base):
     description: Mapped[str | None] = mapped_column(String(500), nullable=True)
     price: Mapped[Decimal] = mapped_column(
         Numeric(10, 2), nullable=False
-    )  # переделал на другой тип
+    )
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, default=0.0)
     is_active: Mapped[bool] = mapped_column(default=True)
     seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
 
@@ -29,4 +31,5 @@ class Product(Base):
     )
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
-    seller = relationship("User", back_populates="products")
+    seller: Mapped["User"] = relationship("User", back_populates="products")
+    reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product")
---
+++ app/models/reviews.py
@@ -0,0 +1,58 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    ForeignKey,
+    func,
+    Integer,
+    Text,
+    CheckConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base
+
+if TYPE_CHECKING:
+    from app.models.users import User
+    from app.models.products import Product
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey(column="users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    product_id: Mapped[int] = mapped_column(
+        ForeignKey(column="products.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    comment: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True,
+    )
+    comment_date: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        server_default=func.now(),
+    )
+    grade: Mapped[int] = mapped_column(
+        Integer,
+        CheckConstraint("grade >= 1 AND grade <=5"),
+        nullable=False,
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+    )
+    author: Mapped["User"] = relationship(
+        "User",
+        back_populates="reviews",
+    )
+    product: Mapped["Product"] = relationship(
+        "Product",
+        back_populates="reviews",
+    )
